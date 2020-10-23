28e06655edaa69099ba26463bc77c371f1633444
Stian Grønås <42640705+hvl578007@users.noreply.github.com>
2020-10-23 14:42:04 +0200

    fiksa skygge i heile terrenget

--- js/lights/LysLager.js
+++ js/lights/LysLager.js
@@ -18,10 +18,16 @@ export default class LysLager {
         retningslys.castShadow = true;
 
         //Set up shadow properties for the light
-        retningslys.shadow.mapSize.width = 2048;
-        retningslys.shadow.mapSize.height = 2048;
+        //litt høg res på shadow texture... 8k ;)
+        retningslys.shadow.mapSize.width = 8192;
+        retningslys.shadow.mapSize.height = 8192;
         retningslys.shadow.camera.near = 0.5;
-        retningslys.shadow.camera.far = 3000;
+        retningslys.shadow.camera.far = 800;
+        //width av terreng/2 gir ca. ok?
+        retningslys.shadow.camera.left = -300;
+        retningslys.shadow.camera.right = 300;
+        retningslys.shadow.camera.top = 300;
+        retningslys.shadow.camera.bottom = -300;
 
         // Set direction
         retningslys.target.position.set(0, 15, 0);
