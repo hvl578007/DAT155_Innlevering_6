d5e3a84e8bfc2f757cfc1b11c0d90e2fc775c2de
577854 <577854@hvl.stud.no>
2020-10-26 13:07:11 +0100

    skydome ny

--- js/skydome/Skydome.js
+++ js/skydome/Skydome.js
@@ -1,15 +1,14 @@
 "use strict";
 
-import { Mesh, MeshPhongMaterial, SphereGeometry, TextureLoader, ShaderMaterial } from "../lib/three.module.js";
+import { Mesh, MeshBasicMaterial, SphereGeometry, TextureLoader, ShaderMaterial } from "../lib/three.module.js";
 
 export default class Skydome extends Mesh {
 
     constructor(){
 
         let skydomeGeometry = new SphereGeometry(350,64,64);
-        let skydomeMaterial = new MeshPhongMaterial({
-            color: 0x808080,
-            map: new TextureLoader().load('./resources/textures/grass_02.png')
+        let skydomeMaterial = new MeshBasicMaterial({
+            map: new TextureLoader().load('./resources/textures/sky.jpg')
         });
 
         super(skydomeGeometry, skydomeMaterial);
