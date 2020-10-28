94c6f585a7127c9f63c280505fe075d9057028ba
Stian Grønås <42640705+hvl578007@users.noreply.github.com>
2020-10-28 14:50:12 +0100

    la til stein

---
+++ js/terrain/objects/Stein.js
@@ -0,0 +1,30 @@
+"use strict";
+
+import { BoxBufferGeometry, CubeGeometry, Mesh, MeshStandardMaterial, SphereBufferGeometry, TextureLoader } from "../../lib/three.module.js";
+
+export default class Stein extends Mesh {
+
+    constructor() {
+
+        let steinDisplacement = new TextureLoader().load('./resources/textures/rock/Rock_039_height.png');
+        let steinNormalMap = new TextureLoader().load('./resources/textures/rock/Rock_039_normal.jpg');
+        let steinRough = new TextureLoader().load('./resources/textures/rock/Rock_039_roughness.jpg');
+        let steinTexture = new TextureLoader().load('./resources/textures/rock/Rock_039_baseColor.jpg');
+        let steinAmbientOcclusion = new TextureLoader().load('./resources/textures/rock/Rock_039_ambientOcclusion.jpg');
+
+        let steinGeometri = new BoxBufferGeometry(10, 10, 10, 64, 64, 64);
+
+        let steinMateriale = new MeshStandardMaterial({
+            aoMap: steinAmbientOcclusion,
+            displacementMap: steinDisplacement,
+            normalMap: steinNormalMap,
+            roughnessMap: steinRough,
+            map: steinTexture
+        });
+
+        super(steinGeometri, steinMateriale);
+
+        this.receiveShadow = true;
+        this.castShadow = true;
+    }
+}
