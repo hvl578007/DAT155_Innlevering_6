90c6306d8bed2c0fbdd698c07bb03f83e787cbd9
Stian Grønås <42640705+hvl578007@users.noreply.github.com>
2020-10-28 20:37:16 +0100

    fjerna fog frå skydome

--- js/skydome/Skydome.js
+++ js/skydome/Skydome.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { Mesh, MeshBasicMaterial, SphereGeometry, TextureLoader, ShaderMaterial } from "../lib/three.module.js";
+import { Mesh, MeshBasicMaterial, SphereGeometry, TextureLoader, ShaderMaterial, BackSide } from "../lib/three.module.js";
 
 export default class Skydome extends Mesh {
 
@@ -8,7 +8,9 @@ export default class Skydome extends Mesh {
 
         let skydomeGeometry = new SphereGeometry(550,64,64);
         let skydomeMaterial = new MeshBasicMaterial({
-            map: new TextureLoader().load('./resources/textures/sky.jpg')
+            map: new TextureLoader().load('./resources/textures/sky.jpg'),
+            fog: false,
+            side: BackSide
         });
 
         super(skydomeGeometry, skydomeMaterial);
